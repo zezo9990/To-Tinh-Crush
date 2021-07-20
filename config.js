a824d018f84d2576044c7a8500efc3cee5af5afa
zezo9990 <87707244+zezo9990@users.noreply.github.com>
2021-07-20 20:44:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Tên  người yêu bạn à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
