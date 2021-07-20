b028d4f087acd0fafe8c8e9c60af0fb2ef9efe5e
zezo9990 <87707244+zezo9990@users.noreply.github.com>
2021-07-20 20:56:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,6 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://facebook.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://facebook.com' //messages/t/100014412360846
 }
  
